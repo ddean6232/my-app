53d3850a040573095c0e65623b69cf413016bc5d
Darren Dean <ddean@dtreeind.com>
2023-01-27 15:59:18 -0700

    initial

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Darren W Dean</Text>
+      <Text>Bound for Belize!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0aa",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
